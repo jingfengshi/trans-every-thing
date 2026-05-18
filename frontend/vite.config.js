0e922ae03e0cee52a75a86c306279a49190ded1b
jasperjing <240023810@qq.com>
2026-05-18 18:00:05 +0800

    chore: change frontend port to 3000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
-    port: 5174,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://backend:8000',
